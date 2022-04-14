7e1b259275e815b99950b3550da4e290f727b71b
nihae28 <nepuri28usa@gmail.com>
2022-04-13 20:29:59 -0500

    test

---
+++ src/test/java/com/dbms/onlineairportmanagement/OnlineAirportManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbms.onlineairportmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineAirportManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
