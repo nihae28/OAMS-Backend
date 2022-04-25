abc611ea9ee52834d5b39ced0b6cb4539296fcb3
Shreyavish <37011949+Shreyavish@users.noreply.github.com>
2022-04-25 15:06:59 -0500

    Add files via upload

---
+++ src/main/java/com/dbms/onlineairportmanagement/controller/TestInfoController.java
@@ -0,0 +1,27 @@
+package com.dbms.onlineairportmanagement.controller;
+import com.dbms.onlineairportmanagement.repository.Test_infoRepository;
+import com.dbms.onlineairportmanagement.model.Test_info;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+        import java.util.List;
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/testinfo")
+public class TestInfoController {
+
+    @Autowired
+    private Test_infoRepository test_info;
+
+    @GetMapping("/gettests")
+    public List<Test_info> getAllTestInfo(){
+        return test_info.findAll();
+    }
+
+    // build create employee REST API
+    @PostMapping("/addtestinfo")
+    public Test_info createTest(@RequestBody Test_info ti) {
+        return test_info.save(ti);
+    }
+
+}
