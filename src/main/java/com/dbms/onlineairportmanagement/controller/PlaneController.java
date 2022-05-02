ae7cdfb52f0269a68dda24d82a901bb7c62eb687
Pachipulusu Raghuveer <ptnbvr@gmail.com>
2022-05-02 14:09:49 -0500

    Update PlaneController.java

--- src/main/java/com/dbms/onlineairportmanagement/controller/PlaneController.java
+++ src/main/java/com/dbms/onlineairportmanagement/controller/PlaneController.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/plane-controller")
 public class PlaneController {
